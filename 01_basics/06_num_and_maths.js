467c248944a326bf20a490a926fed9a395d8c308
Rohanbsavasuddi <131452423+Rohanbsavasuddi@users.noreply.github.com>
2025-03-20 10:06:29 +0000

    math and num

---
+++ 01_basics/06_num_and_maths.js
@@ -0,0 +1,38 @@
+//******************Number******************************** */
+
+// const score = 400
+// console.log(score);
+
+// const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+// const anotherNumber = 251.6654
+// console.log(anotherNumber.toPrecision(3));
+
+// const numberThree = 1000000000
+// console.log(numberThree.toLocaleString('en-IN'));
+
+
+
+// ****************Maths****************************************************************
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6)); 
+// console.log(Math.ceil(4.2)); 
+// console.log(Math.floor(4.6)); 
+// console.log(Math.floor(4.6)); 
+// console.log(Math.min(4,5,8,2,7,6,8,2,6,8,6)); 
+// console.log(Math.max(4,5,8,2,7,6,8,2,6,8,6)); 
+
+console.log(Math.random());
+console.log((Math.random()*10) + 1);
+console.log(Math.floor(Math.random()*10) + 1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1) ) + min)
