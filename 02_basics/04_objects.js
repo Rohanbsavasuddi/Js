e72e47ae940431fbe2254528adc70ef67a5731fd
Rohanbsavasuddi <131452423+Rohanbsavasuddi@users.noreply.github.com>
2025-04-09 07:33:17 +0000

    done with objects

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -48,12 +48,23 @@ const users = [
 ]
 
 // console.log(users[1].email);
-console.log(tinderUser);
+// console.log(tinderUser);
 
-console.log(Object.keys(tinderUser));
-console.log(Object.values(tinderUser));
-console.log(Object.entries(tinderUser));
+// console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderUser));
 
-console.log(tinderUser.hasOwnProperty('isLogged'));
+// console.log(tinderUser.hasOwnProperty('isLogged'));
 
 
+const course = {
+    courseName : "js in hindi",
+    price : "1000",
+    courseInstructor : "hitesh"
+}
+
+// course.courseInstructor
+
+const {courseInstructor: instructor} = course
+
+console.log(instructor);
