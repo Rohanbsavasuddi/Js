f3c8a71c975a53c15b9da9aa7dce349ea5e77f28
Rohanbsavasuddi <131452423+Rohanbsavasuddi@users.noreply.github.com>
2025-04-18 14:09:01 +0000

    IIFE

---
+++ 03_basics/04_iife.js
@@ -0,0 +1,11 @@
+// Immediately Invoked function Expressoion (IIFE)
+
+(function chai (){
+    //named iffe
+    console.log("DB CONNECTED")
+})();
+
+(  (name) => {
+    //unnamed iffe
+    console.log(`DB CONNECTED TWO ${name}`)
+})('Rohan');
