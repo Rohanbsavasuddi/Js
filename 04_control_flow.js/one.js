2095c4873832720ee2baa8e84df737a9e5ee032a
Rohanbsavasuddi <rohansavasuddi7088@gmail.com>
2025-05-03 11:56:40 +0530

    started with conditions

---
+++ 04_control_flow.js/one.js
@@ -0,0 +1,13 @@
+// const isUserLoggedIn = true
+// const temprature = 41
+
+// if(temprature === 41){
+//     console.log("less then 40");
+// }
+// else{
+//     console.log("temprature is greater then 50");
+// }
+
+// console.log("Executed");
+
+// TODO: < (less then), > (greater then), <= (less then or equal), >=(greater then or equal), == (equals), === (equals with datatype), !== (not equals)
