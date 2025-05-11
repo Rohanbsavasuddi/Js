7e0f094ea20d342715877fe83d6cbbdbc0377221
Rohanbsavasuddi <rohansavasuddi7088@gmail.com>
2025-05-11 20:23:34 +0530

    to be continued

---
+++ 04_control_flow.js/truthy.js
@@ -0,0 +1,17 @@
+const userEmail = "h@email,com"
+
+if (userEmail) {
+    console.log("Got user Email");
+}else{
+    console.log("Don't have user email");
+}
+
+
+
+
+// TODO: falsy values
+// TODO:false, 0, -0, BigInt 0n, "", null, undefined, NaN
+
+
+// TODO: truthy values
+// TODO: "0", "false", " ", []
